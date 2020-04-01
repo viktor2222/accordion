8094058b9af9914684e03e93850e88a614a65db8
vpanchuk <v.panchuk@pozitiff.ua>
2020-04-01 16:29:10 +0300

    accordion init

---
+++ index.js
@@ -0,0 +1,10 @@
+let accordion = document.getElementsByClassName("accordion");
+
+for (let i = 0; i < accordion.length; i++) {
+  accordion[i].children[0].onclick = () => {
+    let button = accordion[i].firstElementChild;
+    let text = accordion[i].lastElementChild;
+    text.classList.toggle("accordion-text-block"); // css class accordion-text-block
+    button.classList.toggle("active-accordion"); //  css class active-accordion
+  };
+}
